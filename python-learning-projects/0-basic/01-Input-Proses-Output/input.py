263101e3a6cf229fc4f3eddd9688ae1560d59c4d
KGC- <dapidc2@gmail.com>
2025-09-28 04:52:55 +0700

    Create 01-Input-Proses-Output/input.py

---
+++ python-learning-projects/0-basic/01-Input-Proses-Output/input.py
@@ -0,0 +1,11 @@
+teks = input("teks: ") # "Abc012"
+
+bulat = int(input("input bulat: ")) # 1
+
+desimal = float(input("Input desimal: ")) # 1.0
+
+complexs = complex(input("Input complex: ")) # Output (1 real (1j imag))
+
+split_input = input("Split input dengan coma (,): ").split(",") # Input Dapid,19
+name, age = split_input
+age = int(age.strip()) <- conversi str -> to int
